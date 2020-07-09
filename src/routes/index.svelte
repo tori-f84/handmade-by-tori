e146a6bba8f64ec4e9c41e6dd0bbea40ac1da75b
Matt Brealey <matt@mattbrealey.com>
2020-07-09 10:25:54 +0100

    Comment out meta // Add rel links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,21 @@
 <svelte:head>
   <title>Handmade by Tori</title>
-  <meta name="description" content="The portfolio site of a freelance creative problem solver." />
-  <meta name="keywords" content="space, react, reactjs, threejs, sapper, svelte, sveltejs, ios, swift, 3d, ar, vfx, design"/>
+  <!-- <meta name="description" content="The portfolio site of a freelance creative problem solver." />
+  <meta name="keywords" content="space, react, reactjs, threejs, sapper, svelte, sveltejs, ios, swift, 3d, ar, vfx, design"/> -->
 
   <!-- Open Graph / Facebook -->
-  <meta property="og:type" content="website">
+  <!-- <meta property="og:type" content="website">
   <meta property="og:url" content="https://mattbrealey.com">
   <meta property="og:title" content="Matt Brealey">
   <meta property="og:description" content="The portfolio site of a freelance creative problem solver.">
   <meta property="og:image" content="https://mattbrealey.com/profile.jpg">
 
   <!-- Twitter -->
-  <meta property="twitter:card" content="summary_large_image">
+  <!-- <meta property="twitter:card" content="summary_large_image">
   <meta property="twitter:url" content="https://mattbrealey.com">
   <meta property="twitter:title" content="Matt Brealey">
   <meta property="twitter:description" content="The portfolio site of a freelance creative problem solver.">
-  <meta property="twitter:image" content="https://mattbrealey.com/profile.jpg">
+  <meta property="twitter:image" content="https://mattbrealey.com/profile.jpg"> --> -->
 </svelte:head>
 
 <script context="module">
