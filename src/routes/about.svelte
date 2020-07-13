d9ca78a5b2c50e5327bee87c3adb30dd8986bedc
Matt Brealey <matt@mattbrealey.com>
2020-07-13 20:00:52 +0100

    Add photos to about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,9 +1,16 @@
 <div class="py-6 px-4 sm:px-0 font-light text-gray-800 flex flex-col text-sm sm:text-base ">
-  <p>In my ‘other life’, l've been working for a marine conservation organisation, and have been fortunate enough to travel to many far-flung places, all of which have the sea in common. For me, the ocean has been a life-giver and constant source of inspiration, including for my designs.</p>
+
+  <p class="mt-6">In my ‘other life’, l've been working for a marine conservation organisation, and have been fortunate enough to travel to many far-flung places, all of which have the sea in common. For me, the ocean has been a life-giver and constant source of inspiration, including for my designs.</p>
   <p class="mt-2">I use a lot of chrysoprase chalcedony in my pieces and first fell in love with this stone on a trip to New Zealand. I now source my stones from a family-run small mine in Western Australia, where the stones are hand-cut and hand-finished to a very high quality. Each piece I make with these stones is one of a kind, due to the unique nature of every stone.  The Ancient Greeks considered chrysoprase to be the sacred stone of Aphrodite, whose sacred colour was green and it remains a highly sought-after stone. </p>
   <p class="mt-2">To show-off the beautiful stones, my preferred setting is backless, which takes a little longer to create, but I hope you will agree it is worth it to show off the variations on both sides of the stones - often the back is as beautiful as the front!</p>
 
-  <h2 class="mt-6 text-xl sm:text-2xl sm:font-thin text-toriAccent tracking-wide pb-2">Postage and packaging</h2>
+  <div class="grid grid-cols-3 gap-2 sm:gap-8 mt-12 sm:mx-8">
+    <img class="rounded-lg object-cover" src="./images/about/1.jpg" alt="picture of a beach" />
+    <img class="rounded-lg object-cover" src="./images/about/2.jpg" alt="tori" />
+    <img class="rounded-lg object-cover" src="./images/about/3.jpg" alt="picture of sea urchins" />
+  </div>
+
+  <h2 class="mt-12 text-xl sm:text-2xl sm:font-thin text-toriAccent tracking-wide pb-2">Postage and packaging</h2>
   At the moment I am only shipping to the UK but contact me if you are overseas and would like to purchase or commission an item and find out postage costs. I use recycled packaging wherever possible to try and minimise waste, so don’t be surprised if you get a jiffy bag that originated from Australia!
   Some items are made to order and this will be stated on those items, otherwise items are sold as seen. 
 
